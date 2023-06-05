c52c05883ddedb4008856882f10cd1bd9f21be92
md-toybur-rahman <toyburrahman735@gmail.com>
2023-06-05 11:09:23 +0600

    security lavel

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,13 +1,15 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaUtensils, FaBook, FaUsers } from 'react-icons/fa'
 import useCart from "../Hooks/useCart";
+import useAdmin from "../Hooks/useAdmin";
 
 
 const Dashboard = () => {
     const [cart] = useCart();
 
     // TODO: load data from the server to have dynamic isAdmin based on data
-    const isAdmin = true;
+    // const isAdmin = true;
+    const [isAdmin] = useAdmin();
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" name="" className="drawer-toggle" />
